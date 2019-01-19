fc44bfc9a2ab786bb46a2e6739018e3c7e78cf0d
imabuza <iitmabuza@gmail.com>
2019-01-19 17:09:13 +0200

    added user info

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = goblal.Promise;
+mongoose.Promise = global.Promise;
 // Connect to the database
 mongoose.connect('mongodb://localhost:27017/TodoApp/');
 
@@ -8,26 +8,51 @@ mongoose.connect('mongodb://localhost:27017/TodoApp/');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
     },
     completed:{
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 // Create new todo
 var newTodo = new Todo({
-    text: "This is text",
-    completed: true,
-    completedAt: 1045
+    text: "Smoke some weed "
 });
 
 // save todo
 newTodo.save().then((doc)=>{
-    console.log('Saved doc', doc);
+    console.log(JSON.stringify(doc, undefined, 2));
 }, (e)=>{
     console.log('Could not add doc')
 })
+
+// Create User
+
+var User = mongoose.model('User', {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+var newUser = new User({
+    email: "innocent@innocent.com"
+});
+
+newUser.save().then((doc)=>{
+    console.log(JSON.stringify(doc, undefined,2))
+}, (e)=>{
+    console.log('Couldnt create user')
+})
+
