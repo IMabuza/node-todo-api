2e95484ec1c8e9f4d2235216536850b0dc21668e
imabuza <iitmabuza@gmail.com>
2019-01-20 12:51:38 +0200

    Add /todos route

--- server/server.js
+++ server/server.js
@@ -3,9 +3,23 @@ var bodyParser = require('body-parser');
 
 var {mongooose} = require('./db/mongoose');
 var {User} = require('./models/user');
-var {User} = require('./models/todo');
+var {Todo} = require('./models/todo');
 
 var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) =>{
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
 
 app.listen(3000, ()=> {
     console.log('listening on port 3000');
