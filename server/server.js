f1dd243dc5a3dce12e338817931d3687b4175f40
imabuza <iitmabuza@gmail.com>
2019-01-26 17:14:45 +0200

    add GET route

--- server/server.js
+++ server/server.js
@@ -18,8 +18,18 @@ app.post('/todos', (req, res) =>{
         res.send(doc);
     }, (e) => {
         res.status(400).send(e)
-    })
-})
+    });
+});
+
+// Get todos
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, ()=> {
     console.log('listening on port 3000');
